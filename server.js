92377707bad010eeb6d505113685864db0b9e1bd
Arj09 <arjunkushwah100199@gmail.com>
2023-08-28 20:37:05 +0530

    add

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const errorHandler = require("./middleware/errorhandler");
+const DBconnection = require('./dbConfig/dbConnection')
+const dotenv = require("dotenv").config()
+const cors = require('cors')
+
+
+DBconnection()
+const app = express()
+const port = process.env.PORT || 5001
+
+
+app.use(cors())
+app.use(express.json());
+app.use("/api/user", require('./router/userRouter'))
+app.use(errorHandler);
+
+
+app.listen(port, (req, res)=>{
+    console.log(`server is running on port : ${port}`)
+
+})
